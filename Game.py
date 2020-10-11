cf6d5ec51274f6a6bff2536f913c059c36ccd046
Aaron Crosbie <aaroncrosie@outlook.com>
2020-10-11 19:39:45 +0100

    Near done

--- Game.py
+++ Game.py
@@ -1,17 +1,69 @@
 import pygame
 import random
 import Objects
+import time
+import math
+import tkinter as tk
+from tkinter import messagebox
 
 
-def redrawwindow(win):
+def redrawwindow(win, paddle, paddle1, ball):
     win.fill((0, 0, 0))
+    paddle.draw(win)
+    ball.draw(win)
+    paddle1.draw(win)
+    pygame.display.update()
+    pass
+
+
+def touching(ball, paddle):
+    for i in range(paddle.size[1]):
+        if ball.pos[1] == paddle.pos[1] + i:
+            for i in range(paddle.size[0]):
+                if ball.pos[0] == paddle.pos[0] + i:
+                    ball.bounce(paddle)
+    pass
+
+
+def reset(paddle, paddle1, ball, height):
+    paddle.pos = (10, height/2 - 50)
+    paddle1.pos = (960, height/2 - 50)
+    ball.pos = (500, 250)
+    ball.dir = 0
 
 
 def main():
     width = 1000
     height = 500
-    win = pygame.display.set_mode((height, width))
-    paddle = Objects.paddle((255, 255, 255), 0, 0)
+    score1 = 0
+    score2 = 0
+    win = pygame.display.set_mode((width, height))
+    paddle = Objects.paddle((10, height/2 - 50), 0)
+    paddle1 = Objects.paddle((960, height/2 - 50), 1)
+    ball = Objects.ball((500, 250))
+    clock = pygame.time.Clock()
+    while True:
+        pygame.time.delay(60)
+        clock.tick(10)
+
+        keys = pygame.key.get_pressed()
+        paddle.move(keys)
+        paddle1.move(keys)
+        ball.move()
+        touching(ball, paddle)
+        touching(ball, paddle1)
+        if ball.pos[1] < 0 or ball.pos[1] > 500:
+            ball.bounce(0)
+
+        if ball.pos[0] < 0:
+            score1 = score1 + 1
+            print(score2, ':', score1)
+            reset(paddle, paddle1, ball, height)
+        elif ball.pos[0] > width:
+            score2 = score2 + 1
+            print(score2, ':', score1)
+            reset(paddle, paddle1, ball, height)
+        redrawwindow(win, paddle, paddle1, ball)
 
 
 if __name__ == '__main__':
--- Objects.py
+++ Objects.py
@@ -3,27 +3,52 @@ import pygame
 
 
 class paddle():
-    def __init__(self, colour, pos):
-        self.colour = colour
+    def __init__(self, pos, player):
+        self.colour = 255, 255, 255
         self.pos = pos
+        self.player = player
+        self.speed = 0
+        self.size = (30, 100)
 
     def move(self, keys):
-        if keys[pygame.K_UP]:
-            speed = speed
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        speed = 0
+
+        if keys[pygame.K_DOWN] and self.player == 1:
+            speed = 10
+        elif keys[pygame.K_UP] and self.player == 1:
+            speed = -10
+        elif keys[pygame.K_s] and self.player == 0:
+            speed = 10
+        elif keys[pygame.K_w] and self.player == 0:
+            speed = -10
+        self.pos = (self.pos[0], self.pos[1] + speed)
+        self.speed = speed
 
     def draw(self, surface):
-        pygame.draw.rect(surface, self.colour, self.pos)
+        pygame.draw.rect(surface, self.colour, ((self.pos), (self.size)))
 
 
 class ball():
-    def __init__(self):
-        pass
+    def __init__(self, pos):
+        self.pos = pos
+        self.colour = 255, 255, 255
+        self.dir = 0
+        self.size = 10
+        self.dir1 = 20
 
     def move(self):
-        pass
+        self.pos = (self.pos[0] + self.dir1, self.pos[1] + self.dir)
 
     def draw(self, surface):
-        pass
+        pygame.draw.circle(surface, self.colour, self.pos, self.size)
 
     def bounce(self, paddle):
-        pass
+        if paddle == 0:
+            self.dir = self.dir * -1
+        else:
+            self.dir = paddle.speed
+            self.dir1 = self.dir1 * -1
